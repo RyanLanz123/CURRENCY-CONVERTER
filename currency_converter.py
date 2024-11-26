dc54159da4feff73fc1acc125f7676b0f7591f1c
RyanLanz123 <ryanlanz6@gmail.com>
2024-11-26 20:52:08 +0000

    fully functional, need to update API key

--- currency_converter.py
+++ currency_converter.py
@@ -30,7 +30,55 @@ def exchange_rate(currency1, currency2):
 
     data = response.json()
 
-    printer.pprint(data)
+    if len(data) == 0:
+        print('Invalid currencies')
+        return
+    
+    rate = list(data.values())[0]
+    print(f"{currency1} -> {currency2} {rate}")
+    
+    return rate
 
-data = get_currencies()
-print_currencies(data)
+def convert(currency1, currency2, amount):
+    rate = exchange_rate(currency1, currency2)
+    if rate is None:
+        return
+    
+    try:
+        amount = float(amount)
+    except:
+        print("Invalid amount.")
+        return
+    
+    converted_amount = rate * amount
+    print(f"{amount} {currency1} is equal to {converted_amount} {currency2}")
+    return converted_amount
+
+def main():
+    currencies = get_currencies()
+
+    print("Welcome to the currency converter!")
+    print("List different currencies")
+    print("Convert one currency to another")
+    print("Get exchange rate of 2 currencies")
+
+    while True:
+        command = input("Enter a command (q to quit)L").lower()
+
+        if command == "q":
+            break
+        elif command == "list":
+            print_currencies(currencies)
+        elif command == "convert":
+            currency1 = input("Enter a currency id: ").upper()
+            amount = input(f"Enter an amount in {currency1}: ")
+            currency2 = input("Enter a currency to convert to: ").upper()
+            convert(currency1, currency2, amount)
+        elif command == "rate":
+            currency1 = input("Enter a currency id: ").upper()
+            currency2 = input("Enter a currency to convert to: ").upper()
+            exchange_rate(currency1, currency2)
+        else:
+            print("Unrecognized command!")
+
+main()
